17e0b7cdaf2ffd1af145559c76df600be9a38a07
StivenGonzalezz <grstiven1004@gmail.com>
2025-01-30 00:20:55 -0500

    .

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function toggleClassTask(){
-    let task = document.getElementById("taks1")
+    let task = document.getElementById("task1")
     task.classList.toggle("active")
+}
+
+function createTask(){
+    let taskName = document.getElementById("taskInput")
+    
 }
